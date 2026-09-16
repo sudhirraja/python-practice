7fe25c25f41ba333fac284fc37ba42583d04a01e
Sudhir Raja <sudhirraja@gmail.com>
2026-09-16 00:06:29 -0700

    Week 2: functions exercises 1 and 2

---
+++ week2/ch3_ex1.py
@@ -0,0 +1,5 @@
+def greet_engineer(name):
+    print("Welcome to the lab,", name)
+
+greet_engineer("Sudhir")
+greet_engineer("Priya")
---
+++ week2/ch3_ex2.py
@@ -0,0 +1,3 @@
+def gbps_to_mbps(gbps):
+    return gbps * 1000
+print(gbps_to_mbps(400))
